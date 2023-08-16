8acacd2e9593dd3b9fb0a3450ef612e12ba482b5
Michael Alan Stowe <mastowe94@gmail.com>
2023-08-15 19:02:58 -0500

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+function onFocus(){
+    console.log("Good!")
+}
+
+function onBlur(){
+    console.log("Hey! Eyes on me!")
+}
+
+function EyesOnMe(){
+    return (
+        <>
+            <button
+            onFocus={onFocus}
+            onBlur={onBlur}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
+
+
 
 function Keypad (){
+    function handlChange(event){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <>
+            <form>
+                <input type="password"
+                onChange={handlChange} />
+            </form>
+        </>
     )
 }
 
